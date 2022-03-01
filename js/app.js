51fc8476b923aa92c839574b4dccb14fd4602404
devriazul <engr.riazul@gmail.com>
2022-03-01 23:26:58 +0600

    overflow control

--- js/app.js
+++ js/app.js
@@ -80,7 +80,7 @@ const loadingSpinner = display => {
      phoneInfoContainer.innerHTML = `
 
      <div class="col-md-4" style="background-image:url('${phone.image}');background-repeat: no-repeat; background-position: center center; background-size: fit;">
-          <div style="height:300px"></div>
+          <div style="height:300px overflow:scroll"></div>
       </div>
       <div class="col-md-8">
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -89,8 +89,11 @@ const loadingSpinner = display => {
               <p class="area">Release Date: <span>${phone.releaseDate ? phone.releaseDate : 'No Relesed Date Found!' }</span></p>
               <h6 class="category">Brand: <span>${phone.brand}</span></h6>
               <hr>
-              <h5>Features:</h5>
-              <p class="card-text">Sensors: ${phone.mainFeatures.sensors}...</p>
+              <h5> Main Features:</h5>
+              <p>Storage & Memory: ${phone.mainFeatures.storage}, ${phone.mainFeatures.memory}</p>
+              <p>Display Size: ${phone.mainFeatures.displaySize}</p>
+              <p>Chipset: ${phone.mainFeatures.chipSet}</p>
+              <p class="card-text">Sensors: ${phone.mainFeatures.sensors}</p>
             </div>
         </div>
     </div>    
